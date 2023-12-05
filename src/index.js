f9edffe3c9ead5efe542e35c1bb4a845735d1817
pyplee <dekimiq@gmail.com>
2023-12-05 15:50:24 +0300

    add func for i18n

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './styles.scss';
 import 'bootstrap';
 import initValidateUrl from './view.js';
+import i18nInit from './locales/langIndex.js';
 
 console.log('Hello, World!');
+i18nInit();
 initValidateUrl();
