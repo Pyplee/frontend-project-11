7607dba5189f92b9f364106c4677e1454ff6ee95
pyplee <dekimiq@gmail.com>
2023-12-05 15:51:00 +0300

    add keys for translate

---
+++ src/locales/en.js
@@ -0,0 +1,12 @@
+export default {
+  translation: {
+    // BEGIN (write your solution here)
+    title: 'RSS aggregator',
+    descTitle: 'Start reading RSS today! It\'s easy, it\'s beautiful.',
+    descInput: 'Link RSS',
+    example: 'Example: https://ru.hexlet.io/lessons.rss',
+    notValidLink: 'The link must be a valid URL',
+    button: 'Add',
+    // END
+  },
+};
---
+++ src/locales/ru.js
@@ -0,0 +1,12 @@
+export default {
+  translation: {
+    // BEGIN (write your solution here)
+    title: 'RSS агрегатор',
+    descTitle: 'Начните читать RSS сегодня! Это легко, это красиво.',
+    descInput: 'Ссылка RSS',
+    example: 'Пример: https://ru.hexlet.io/lessons.rss',
+    notValidLink: 'Ссылка должна быть валидным URL',
+    button: 'Добавить',
+    // END
+  },
+};
