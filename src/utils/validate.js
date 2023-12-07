0bfcf5900d49c3d6496772de8208d3957a91fcd3
pyplee <dekimiq@gmail.com>
2023-12-07 19:19:33 +0300

    commented out code that will be needed in the future

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,21 +1,28 @@
 import * as yup from 'yup';
 
-export default (url, urlsList, i18n) => {
-  yup.setLocale({
-    string: {
-      url: i18n.t('form.errors.notValidUrl'),
-    },
-    mixed: {
-      required: i18n.t('form.errors.required'),
-      notOneOf: i18n.t('form.errors.notUniqueUrl'),
-    },
-  });
+export default (url) => { // (url, urlsList, i18n)
+  // yup.setLocale({
+  //   string: {
+  //     url: i18n.t('form.errors.notValidUrl'),
+  //   },
+  //   mixed: {
+  //     required: i18n.t('form.errors.required'),
+  //     notOneOf: i18n.t('form.errors.notUniqueUrl'),
+  //   },
+  // });
 
   const schema = yup
     .string()
     .required()
-    .url()
-    .notOneOf(urlsList);
+    .url();
+    //  .notOneOf(urlsList);
 
   return schema.validate(url);
+
+/*
+   const schema = yup.object({
+     url: yup.string().url(),
+   });
+   return schema.validate({ url });
+  */
 };
