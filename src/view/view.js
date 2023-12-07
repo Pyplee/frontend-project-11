2439618e925fd23ccaf9a584428f947eb6b74bb0
pyplee <dekimiq@gmail.com>
2023-12-07 18:37:49 +0300

    move view.js in dir view

---
+++ src/view/view.js
@@ -0,0 +1,15 @@
+// import onChange from 'on-change';
+// import validateUrl from './validate.js';
+
+// const state = {
+//   validStatus: {
+//     valid: null,
+//   },
+//   InputValue: {
+//     value: '',
+//   },
+// };
+// // const input = document.getElementById('url-input'); !MOVE
+// // const watchedStateInput = onChange(state.InputValue, validateUrl);
+
+// // watchedStateInput.value = e.target.value;
