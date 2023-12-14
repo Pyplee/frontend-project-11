4ad717ccc9b8628f83f1530f258ccb6ad84c8a22
pyplee <dekimiq@gmail.com>
2023-12-14 18:46:07 +0300

    add yup setlocate

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,15 +1,15 @@
 import * as yup from 'yup';
 
-export default (url) => { // (url, urlsList, i18n)
-  // yup.setLocale({
-  //   string: {
-  //     url: i18n.t('form.errors.notValidUrl'),
-  //   },
-  //   mixed: {
-  //     required: i18n.t('form.errors.required'),
-  //     notOneOf: i18n.t('form.errors.notUniqueUrl'),
-  //   },
-  // });
+export default (url, i18n) => { // (url, urlsList, i18n)
+  yup.setLocale({
+    string: {
+      url: i18n.t('meassages.errorNotValid'),
+    },
+    // mixed: {
+    //   required: i18n.t('form.errors.required'),
+    //   notOneOf: i18n.t('form.errors.notUniqueUrl'),
+    // },
+  });
 
   const schema = yup
     .string()
