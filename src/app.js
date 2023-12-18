e97383ab1b6416acfbca474bb4c87c3d23dcbd94
pyplee <dekimiq@gmail.com>
2023-12-18 15:15:24 +0300

    rewrite change state

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
+import _ from 'lodash';
 import validateUrl from './utils/validate.js';
 import initView from './view/view.js';
+import fetch from './utils/fetch.js';
+import parse from './utils/parse.js';
 
 const globalState = {
   rssForm: {
     error: null,
     valid: null,
+  },
+  data: {
+    feeds: [],
+    posts: [],
   },
 };
 
@@ -17,16 +24,35 @@ const app = (elsDOM, i18n) => {
     const formData = new FormData(e.target);
     const url = formData.get('url');
     validateUrl(url, i18n)
-      .then((response) => {
-        console.log(response);
+      .then(() => {
         watchedState.rssForm.error = null;
         watchedState.rssForm.valid = true;
+        const valueInput = elsDOM.input.value;
+        return fetch(valueInput);
+        // return { data: '12345' };
+      })
+      .then((response) => response.data)
+      .then((data) => parse(data.contents))
+      .then((data) => {
+        const [feeds, posts] = data;
+        feeds[0].id = _.uniqueId();
+        posts.forEach((item) => {
+          item.id = _.uniqueId();
+        });
+        const otherFeeds = globalState.data.feeds;
+        const otherPosts = globalState.data.posts;
+        watchedState.data = {
+          feeds: [...otherFeeds, ...feeds],
+          posts: [...otherPosts, ...posts],
+        };
       })
       .catch((err) => {
-        console.log(err);
-        console.log(err.message);
-        watchedState.rssForm.error = err.message;
-        watchedState.rssForm.valid = false;
+        if (err.message === 'Network Error') {
+          watchedState.rssForm.error = i18n.t('meassages.networkError');
+        } else {
+          watchedState.rssForm.error = err.message;
+          watchedState.rssForm.valid = false;
+        }
       });
   });
 };
