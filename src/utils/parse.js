00ab71f28374db69727b9f285cf626a6f328f152
pyplee <dekimiq@gmail.com>
2023-12-18 15:16:33 +0300

    delete error catching

--- src/utils/parse.js
+++ src/utils/parse.js
@@ -1,21 +1,16 @@
 export default (response) => {
   const parser = new DOMParser();
   const parsed = parser.parseFromString(response, 'application/xml');
-  const errorNode = parser.querySelector('parsererror');
-  if (errorNode) {
-    console.log('parse error ??');
-    return null;
-  }
   const channel = parsed.documentElement.children[0];
   const coll = [...channel.children];
   const [title, description] = coll;
-  const fids = [{ title, description }];
+  const feeds = [{ title: title.textContent, description: description.textContent }];
   const posts = [];
   coll.forEach((el) => {
     if (el.tagName === 'item') {
       const [tl, , link, desc] = [...el.children];
-      posts.push({ title: tl, link, description: desc });
+      posts.push({ title: tl.textContent, link: link.textContent, description: desc.textContent });
     }
   });
-  return [fids, posts];
+  return [feeds, posts];
 };
