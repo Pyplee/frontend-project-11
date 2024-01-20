d9f18acf803ade980ab76c74c82a141abce942c5
pyplee <dekimiq@gmail.com>
2024-01-20 15:54:22 +0300

    Added a method to check if a feed exists in the available feeds

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,21 +1,21 @@
 import * as yup from 'yup';
 
-export default (url, i18n) => { // (url, urlsList, i18n)
+export default (url, urlsList, i18n) => { // (url, urlsList, i18n)
   yup.setLocale({
     string: {
       url: i18n.t('meassages.errorNotValid'),
     },
-    // mixed: {
-    //   required: i18n.t('form.errors.required'),
-    //   notOneOf: i18n.t('form.errors.notUniqueUrl'),
-    // },
+    mixed: {
+      // required: i18n.t('form.errors.required'),
+      notOneOf: i18n.t('meassages.notUniqueUrl'),
+    },
   });
 
   const schema = yup
     .string()
     .required()
-    .url();
-    //  .notOneOf(urlsList);
+    .url()
+    .notOneOf(urlsList);
 
   return schema.validate(url);
 
