878b2333778a24e6ef0bcbb96583db8b093dc261
pyplee <dekimiq@gmail.com>
2024-01-20 17:00:25 +0300

    edit structure rss object

--- src/utils/parse.js
+++ src/utils/parse.js
@@ -3,14 +3,19 @@ export default (response, url) => {
   const parsed = parser.parseFromString(response, 'application/xml');
   const channel = parsed.documentElement.children[0];
   const coll = [...channel.children];
+  console.log(coll);
   const [title, description] = coll;
-  const feeds = [{ title: title.textContent, description: description.textContent, url }];
-  const posts = [];
+  const items = [];
   coll.forEach((el) => {
     if (el.tagName === 'item') {
       const [tl, , link, desc] = [...el.children];
-      posts.push({ title: tl.textContent, link: link.textContent, description: desc.textContent });
+      items.push({ title: tl.textContent, link: link.textContent, description: desc.textContent });
     }
   });
-  return [feeds, posts];
+  return {
+    title: title.textContent,
+    description: description.textContent,
+    url,
+    items,
+  };
 };
