1c07f37806ad954fbcf76cdd198497eea87de772
pyplee <dekimiq@gmail.com>
2024-01-21 17:54:44 +0300

    rename file and add func for send messages success

--- src/view/renderStatus.js
+++ src/view/renderStatus.js
@@ -2,6 +2,10 @@ export default (message, elsDOM) => {
   if (message === null) {
     elsDOM.textInfo.classList.remove('text-danger');
     elsDOM.textInfo.textContent = '';
+  } else if (message === 'RSS успешно загружен') {
+    elsDOM.textInfo.classList.remove('text-danger');
+    elsDOM.textInfo.classList.add('text-success');
+    elsDOM.textInfo.textContent = message;
   } else {
     elsDOM.textInfo.classList.add('text-danger');
     elsDOM.textInfo.textContent = message;
