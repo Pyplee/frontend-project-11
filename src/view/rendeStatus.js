d1a033dc19f05b5a126c2d87fad2be8aff9ea44f
pyplee <dekimiq@gmail.com>
2024-01-21 18:18:40 +0300

    del file

--- src/view/rendeStatus.js
+++
@@ -1,5 +0,0 @@
-export default (elsDOM) => {
-  elsDOM.textInfo.classList.remove('text-danger');
-  elsDOM.textInfo.classList.add('text-success');
-  elsDOM.textInfo.textContent = 'RSS успешно загружен';
-};
