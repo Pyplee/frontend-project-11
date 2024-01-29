5566a47f49dc1453ebedf8f423b3be821885ac08
pyplee <dekimiq@gmail.com>
2024-01-30 00:13:24 +0300

    moved more function in other files

--- src/app.js
+++ src/app.js
@@ -1,9 +1,6 @@
-import _ from 'lodash';
-import validateUrl from './utils/validate.js';
 import initView from './view/view.js';
-import fetch from './utils/fetch.js';
-import parse from './utils/parse.js';
-import renderChecked from './view/renderChecked.js';
+import mainFunc from './utils/main.js';
+import { clearUpdate } from './utils/update.js';
 
 const globalState = {
   rssForm: {
@@ -15,86 +12,6 @@ const globalState = {
     checked: [],
   },
 };
-
-let timeoutID;
-
-function initEventButton(watchedState) {
-  const myModal = document.querySelector('#modal');
-  const btnForModal = document.querySelectorAll('[data-bs-toggle="modal"]');
-
-  btnForModal.forEach((btnM) => {
-    btnM.addEventListener('click', (el) => {
-      const btn = el.target;
-      const { id } = btn.dataset;
-      const allPosts = globalState.rss.channels.map((channel) => channel.items).flat();
-      const post = allPosts.find((item) => item.id === id);
-      const titleModal = myModal.querySelector('h5');
-      const descModal = myModal.querySelector('.text-break');
-      const linkModal = myModal.querySelector('a');
-      linkModal.href = post.link;
-      titleModal.textContent = post.title;
-      descModal.textContent = post.description;
-      watchedState.rss.checked.push(id);
-    });
-  });
-}
-
-async function updateData(watchedState) {
-  try {
-    let checkNewData = false;
-    const oldData = globalState.rss.channels;
-    const contentPromises = oldData.map((channel) => fetch(channel.url));
-    const contents = await Promise.all(contentPromises);
-    const newData = contents.map((response) => {
-      const { data } = response;
-      return parse(data.contents, data.status.url);
-    });
-    const result = oldData.map((oldChannel, index) => {
-      const { items } = oldChannel;
-      const oldTitles = items.map((obj) => obj.title);
-      const newChannel = newData[index];
-      const newItems = newChannel.items;
-      const itemDifferences = newItems.filter((el) => {
-        const { title } = el;
-        return !oldTitles.includes(title);
-      });
-      if (itemDifferences.length) {
-        checkNewData = true;
-        const channelWithDifferences = oldChannel;
-        const itemDifferencesWithId = itemDifferences.map((obj) => {
-          obj.id = _.uniqueId();
-          return obj;
-        });
-        channelWithDifferences.items = [...itemDifferencesWithId, ...items];
-        return channelWithDifferences;
-      }
-      return oldChannel;
-    });
-    if (checkNewData) {
-      watchedState.rss.channels = result;
-      initEventButton(watchedState);
-      renderChecked(globalState.rss.checked);
-    }
-    timeoutID = window.setTimeout(() => {
-      updateData(watchedState);
-    }, 5000);
-  } catch (err) {
-    console.log(err);
-    timeoutID = window.setTimeout(() => {
-      updateData(watchedState);
-    }, 5000);
-  }
-}
-
-function delayedUpdate(watchedState) {
-  timeoutID = window.setTimeout(() => {
-    updateData(watchedState);
-  }, 5000);
-}
-
-function clearUpdate() {
-  window.clearTimeout(timeoutID);
-}
 
 function getUrlList() {
   const { channels } = globalState.rss;
@@ -114,29 +31,7 @@ const app = (elsDOM, i18n) => {
     clearUpdate();
     const formData = new FormData(e.target);
     const url = formData.get('url');
-    validateUrl(url, getUrlList(), i18n)
-      .then(() => {
-        watchedState.rssForm.error = null;
-        watchedState.rssForm.valid = true;
-        const valueInput = elsDOM.input.value;
-        return fetch(valueInput);
-      })
-      .then((response) => response.data)
-      .then((data) => parse(data.contents, url))
-      .then((data) => {
-        data.items.forEach((item) => {
-          item.id = _.uniqueId();
-        });
-        const oldChannels = globalState.rss.channels;
-        watchedState.rss.channels = [...oldChannels, data];
-        initEventButton(watchedState);
-        renderChecked(globalState.rss.checked);
-        delayedUpdate(watchedState);
-      })
-      .catch((err) => {
-        watchedState.rssForm.error = err.message;
-        watchedState.rssForm.valid = false;
-      });
+    mainFunc(url, getUrlList, i18n, watchedState, globalState, elsDOM);
   });
 };
 
