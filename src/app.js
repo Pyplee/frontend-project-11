6df62633917613d962fad2bf1e4a2ba603a0f1ea
pyplee <dekimiq@gmail.com>
2024-04-09 14:56:46 +0300

    mooved state in function

--- src/app.js
+++ src/app.js
@@ -2,19 +2,8 @@ import initView from './view/view.js';
 import mainFunc from './utils/main.js';
 import { clearUpdate } from './utils/update.js';
 
-const globalState = {
-  rssForm: {
-    error: null,
-    valid: null,
-  },
-  rss: {
-    channels: [],
-    checked: [],
-  },
-};
-
-function getUrlList() {
-  const { channels } = globalState.rss;
+function getUrlList(state) {
+  const { channels } = state.rss;
   if (!channels.length) {
     return [];
   }
@@ -23,6 +12,17 @@ function getUrlList() {
 }
 
 const app = (elsDOM, i18n) => {
+  const globalState = {
+    rssForm: {
+      error: null,
+      valid: null,
+    },
+    rss: {
+      channels: [],
+      checked: [],
+    },
+  };
+
   const watchedState = initView(globalState, elsDOM, i18n);
 
   const form = document.querySelector('.rss-form');
