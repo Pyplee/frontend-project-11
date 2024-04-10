d2fedbeee7cf27760fb84e2db40888479b1f8a1c
pyplee <dekimiq@gmail.com>
2024-04-10 14:54:07 +0300

    added throw error and refact

--- src/utils/parse.js
+++ src/utils/parse.js
@@ -3,37 +3,21 @@ function tryParseXML(xmlString) {
   const parsererrorNS = parser.parseFromString('INVALID', 'application/xml').getElementsByTagName('parsererror')[0].namespaceURI;
   const dom = parser.parseFromString(xmlString, 'application/xml');
   if (dom.getElementsByTagNameNS(parsererrorNS, 'parsererror').length > 0) {
-    throw new Error('The resource does not contain valid RSS');
+    throw new Error(dom.getElementsByTagNameNS(parsererrorNS, 'parsererror')[0].textContent);
   }
   return dom;
 }
 
 export default (response, url) => {
-  const parsed = tryParseXML(response);
-  const channel = parsed.documentElement.children[0];
+  const dom = tryParseXML(response);
+  const channel = dom.documentElement.children[0];
   const coll = [...channel.children];
   const [title, description] = coll;
-  const items = [];
-  coll.forEach((el) => {
-    if (el.tagName === 'item') {
-      let tl;
-      let link;
-      let desc;
-      const children = [...el.children];
-      children.forEach((element) => {
-        if (element.tagName === 'title') {
-          tl = element;
-        }
-        if (element.tagName === 'description') {
-          desc = element;
-        }
-        if (element.tagName === 'link') {
-          link = element;
-        }
-      });
-      items.push({ title: tl.textContent, link: link.textContent, description: desc.textContent });
-    }
-  });
+  const items = Array.from(dom.querySelectorAll('item')).map((el) => ({
+    title: el.querySelector('title').textContent,
+    link: el.querySelector('link').textContent,
+    description: el.querySelector('description').textContent,
+  }));
   return {
     title: title.textContent,
     description: description.textContent,
